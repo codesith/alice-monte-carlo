e71052ab50d04d7c8437ee242ba5f126839582d7
codesith <codesith@gmail.com>
2016-10-08 13:40:15 -0700

    Format output

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ func main() {
 		coin.Coin{coin.H, coin.H},
 		coin.Coin{coin.T, coin.T}}
 
-	trails := 1000000000
+	trials := 1000000000
 	overall := 0
 	success := 0
-	for i := 0; i < trails; i++ {
+	for i := 0; i < trials; i++ {
 		p.AddAll(coins)
 		// draw a single coin
 		c := p.PickRandom()
@@ -32,6 +32,14 @@ func main() {
 			}
 		}
 		p.RemoveAll()
+		printProgress(trials, i)
 	}
-	fmt.Println(float64(success) / float64(overall))
+	fmt.Printf("\rFinal result is %.2f%%\n", float64(success)/float64(overall)*100)
+}
+
+func printProgress(trials int, i int) {
+	bucketSize := trials / 20
+	if i%bucketSize == 0 {
+		fmt.Printf("\r%d%%", (i/bucketSize+1)*5)
+	}
 }
